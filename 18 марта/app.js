d8d5ef65a1c51c8085f7d3597ec37bc62ce97c85
Caseyys <kseniasidorenko99@mail.ru>
2024-03-18 02:21:07 +0300

    работа с вс кодом

---
+++ 18 марта/app.js
@@ -0,0 +1,6 @@
+let college = [
+    {name: "fon", age: 12, group: "sp-4-23"},
+    {name: "ada", age: 15, group: "sp-1-22"},
+    {name: "wayn", age: 19, group: "sp-2-20"}
+]
+console.log(college[2].group)
